f56d32c76a239b31960b517313f42260151d566d
roelver <roel@romaniflo.nl>
2016-03-20 18:40:38 +0100

    Regenerated with Mongoose

--- src/services/camper/hooks/index.js
+++ src/services/camper/hooks/index.js
@@ -6,9 +6,9 @@ const auth = require('feathers-authentication').hooks;
 
 exports.before = {
   all: [
-    auth.verifyToken(),
-    auth.populateUser(),
-    auth.requireAuth()
+    // auth.verifyToken(),
+    // auth.populateUser(),
+    // auth.requireAuth()
   ],
   find: [],
   get: [],
